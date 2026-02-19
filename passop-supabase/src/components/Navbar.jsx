668c34fec55354cd47c9c7120e6b861593fde2dd
JAY CHAUHAN <cse.190840131009@gmail.com>
2026-02-19 16:13:38 +0530

    update ui

--- passop-supabase/src/components/Navbar.jsx
+++ passop-supabase/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Navbar = () => {
   return (
            <div>
-            <nav className='bg-slate-800 text-white '>
+            <nav className='bg-black text-white '>
                 <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
 
                     <div className="logo font-bold text-white text-2xl">
